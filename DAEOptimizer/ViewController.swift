059a50f6e6b706c1d2f5150eca2141ea07b2d0cf
Ke <keluo@kes-macbook-pro.local>
2018-04-15 15:12:07 -0600

    Initial Commit

---
+++ DAEOptimizer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  DAEOptimizer
+//
+//  Created by Ke Luo on 4/15/18.
+//  Copyright © 2018 Ke Luo. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
